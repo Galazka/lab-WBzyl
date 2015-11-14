f6b73304dd84516a17dad9b1e52321e4a1cd589a
Tomasz Galazka <tomekgalazka@gmail.com>
2015-11-14 11:16:22 +0100

    Initial commit

---
+++ 2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void print_pattern(int ile_gw) {
+
+  for(int i = 0 ; i < ile_gw ; i ++){
+  printf("* ");
+}
+printf("\n");
+for(int i = 0; i < ile_gw; i ++){
+  printf(" *");
+}
+printf("\n");
+}
+
+int main() {
+int n=4 ;
+  for(int i = 0 ); i < 4; i ++) {
+    print_pattern(8);
+}
+
+
+}
---
+++ Makefile
@@ -0,0 +1,9 @@
+
+C=gcc
+
+CFLAGS=-Wall -std=c11
+LDLIBS= -lm
+
+clean:
+	rm -f *.o *~ 0 1 2 3 4 5
+0: 0c
